54230390d2918b966c41eb3df109f1b1eb300a3a
anna.fontanka <anna.fontanka@mail.ru>
2024-07-05 17:11:40 +0300

    ProfileDone

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var discriptionLabel: UILabel!
+    @IBOutlet weak var photoProfile: UIImageView!
+    @IBOutlet weak var loginNameLabel: UILabel!
+}
+
+
+
+
