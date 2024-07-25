702180d6c60a1c1b2ef91473ff4389d4da0ed69b
anna.fontanka <anna.fontanka@mail.ru>
2024-07-25 23:05:40 +0300

    Present View Controller

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -1,6 +1,24 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
-    @IBOutlet var imageView: UIImageView!
-}
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    }
+
+    
